771a39b087174851d223ee4870390cee1911fe97
mzetasmith <36317331+mzetasmith@users.noreply.github.com>
2024-12-11 10:42:36 -0800

    Update and rename opencv_test.py to image_process.py
    changed to be the image process stuff only

--- image_process.py
+++ image_process.py
@@ -3,31 +3,20 @@ from imutils import contours
 import os
 import numpy as np
 
-#winner so far
-"""
-lace work matrix. The first column is the image to match, the second is the right side stitch, the third is the wrong side,
-fourth is the lean (r= right, l = left, n = neutral), fifth is stitches gained or loss
-"""
-
-lace_matrix = np.array([["images\knit-symbol-P-on-RS.png", "p", "k", "n", "0"], ["images\knit-symbol-Yarn-over.png", "yo", "yop", "n", "1"], ["images\knit-symbol-SSK-on-RS.png", "ssk", "p2tog", "l", "-1"], ["images\knit-symbol-K2tog-on-RS.png", "k2tog", "ssp", "r", "-1"], ["images\knit-symbol-SSP-on-RS.png", "ssp", "k2tog", "l", "-1"], ], dtype= "object")
-"""
-cable matrix. The first column is the image to match, the second is the right side stitch, third is wrong side
-"""
-
-cable_matrix = np.array([[]], dtype = "object")
+import knit_compile
 
 def convert(image_path, stitches_per_row, row_number, x_position, y_position):
     stitch_matrix, thresh, gray = process_chart(image_path)
     chart_type = determine_chart(image_path)
     if (chart_type == 0):
         punch_card = color_punch(stitch_matrix, thresh, gray)
-        return make_instructions_color(punch_card, stitches_per_row, row_number, x_position, y_position)
+        return knit_compile.make_instructions_color(punch_card, stitches_per_row, row_number, x_position, y_position)
     elif (chart_type == 1):
         punch_card = lace_punch(stitch_matrix, thresh, gray)
-        return make_instructions_lace(punch_card, stitches_per_row, row_number, x_position, y_positon)
+        return knit_compile.make_instructions_lace(punch_card, stitches_per_row, row_number, x_position, y_positon)
     elif (chart_type == 2):
         punch_card = cable_punch(stitch_matrix, thresh, gray)
-        return make_instructions_cable(punch_card, stitches_per_row, row_number, x_position, y_positon)
+        return knit_compile.make_instructions_cable(punch_card, stitches_per_row, row_number, x_position, y_positon)
     else:
         return "error"
 
@@ -42,8 +31,6 @@ def process_chart(image_path):
     # Read the image
     image = cv2.imread(image_path)
     big_image= cv2.resize(image, None, fx = 3.0, fy = 3.0, interpolation = cv2.INTER_LINEAR)
-    cv2.imshow("Labeled Grid", big_image)
-    cv2.waitKey(0)
     
     #image processing
     gray = cv2.cvtColor(big_image, cv2.COLOR_BGR2GRAY)
@@ -181,7 +168,6 @@ def lace_punch(stitches, image, gray):
                 punch_card_row.append('k')
         punch_card.append(punch_card_row)
         punch_card_row = []
-    print(punch_card)
     return punch_card
 
 """
@@ -230,7 +216,6 @@ def cable_punch(stitches, image, gray):
                 punch_card_row.append('k')
         punch_card.append(punch_card_row)
         punch_card_row = []
-    print(punch_card)
     return punch_card
 
 """
@@ -266,251 +251,11 @@ def count_row_stitches(knit_contours):
             rownum +=1
     return rownum
 
-"""
-checks if the numbers are valid
-"""
-
-def check_valid(punch_card, stitches_per_row, row_number):
-    pattern_rows_count = len(punch_card)
-    pattern_stitches_per_row = len(punch_card[0])
-    spr = int(stitches_per_row)
-    nor = int(row_number)
-    if (nor == spr == -1):
-        return True
-    elif (nor == -1) and (spr > pattern_rows_count -1):
-        return True
-    elif (spr == -1) and (nor > stitches_per_row -1):
-        return True
-    elif (nor < pattern_rows_count):
-        return False
-    elif (spr < pattern_stitches_per_row):
-        return False
-    else:
-        return True
-
-"""
-calculates the repeat stitches
-"""
-
-def calc_repeats(stitches, row):
-    if (stitches != -1):
-        repeats = stitches//row
-    else:
-        repeats = 1
-    return repeats
-
-"""
-checks if there are excess stitches or is needed
-"""
-
-def is_excess(stitches, rows):
-    if (stitches % rows == 0):
-        return False
-    else:
-        if (stitches == -1):
-            return False
-        else:
-            return True
-
-"""
-calculates the excess stitches
-"""
-def xs_stitches(stitches, rows, position):
-    xs = stitches % rows
-    xs_left = 0
-    xs_right = 0
-    if (position == "left") or (position == "up"):
-        xs_left = xs
-    elif (position == "right") or (position == "down"):
-        xs_right = xs
-    elif (position == "center"):
-        xs_left = xs//2
-        xs_right = xs-xs_left
-    else:
-        xs_left = 0
-        xs_right = 0
-    
-    return xs_left, xs_right
-
-"""
-all excess calcs in one
-"""
-
-def excess_stitch(stitches, rows, position):
-    if (is_excess(stitches, rows)):
-        return xs_stitches(stitches, rows, position)
-    else:
-        return 0, 0
-    
-"""
-Creates knitting instructions assuming a colorwork pattern
-"""
-def make_instructions_color(punch_card, stitches_per_row, row_number, x_position, y_position):
-    number = 1
-    row = 1
-    previous = -1
-    instructions = ""
-    row_instructions = ""
-    r_selvedge = ""
-    l_selvedge = ""
-    knit_or_purl = "k"
-    
-    if (check_valid(punch_card, stitches_per_row, row_number)):
-        x_repeats = calc_repeats(int(stitches_per_row), len(punch_card))
-        y_repeats = calc_repeats(int(row_number), len(punch_card[0]))
-        x_xs_left, x_xs_right = excess_stitch(int(stitches_per_row), len(punch_card), x_position)
-        y_xs_up, y_xs_down = excess_stitch(int(row_number), len(punch_card[0]), y_position)
-
-        if (y_xs_down > 0):
-            instructions += "Knit " + str(y_xs_down) + " rows stockinette \n \n"
-        
-        if (x_xs_right > 0):
-            r_selvedge = " (selvedge: k" + "A" + str(x_xs_right) + ") "
-        if (x_xs_left > 0):
-            l_selvedge = " (selvedge: k"  + "A" + str(x_xs_left) + ") " 
-
-        instructions += "Pattern Start: \n"
-        for i in punch_card:
-             
-            for j in i:
-                if j == previous:
-                    number += 1
-                else:
-                    if j == 0:
-                        if (previous != -1):
-                            row_instructions += knit_or_purl + "B" + str(number) + ", "
-                            number = 1
-                        previous = j
-                    elif j == 1: 
-                        if (previous != -1):
-                            row_instructions += knit_or_purl + "A" + str(number) + ", "
-                            number = 1
-                        previous = j
-            end_catch = "A" if previous%2 == 0 else "B"
-            row_instructions += knit_or_purl + end_catch + str(number)
-            if (x_repeats > 1):
-                instructions += "Row " + str(row) + ": " + l_selvedge + "*" + row_instructions + "* " + str(x_repeats) + " times" + r_selvedge + "\n"
-            else:
-                instructions += "Row " + str(row) + ": " + l_selvedge + row_instructions + r_selvedge + "\n"        
-            knit_or_purl = "k" if row%2 == 0 else "p"
-            previous = -1
-            number = 1
-            row_instructions = ""
-            row +=1
-        if(y_repeats > 1):
-            instructions += "\nRepeat pattern for " + str(y_repeats) + " number of times \n"
-        if (y_xs_up > 0):
-            instructions += "\nKnit " + str(y_xs_up) + " rows in stockinette"
-    else:
-        instructions = "The measurements given do not work."
-    return instructions
-
-"""
-Make instructions assuming on a lace pattern
-"""    
-
-def make_instructions_lace(punch_card, stitches_per_row, row_number, x_position, y_position):
-    number = 1
-    row = 1
-    previous = -1
-    instructions = ""
-    row_instructions = ""
-    r_selvedge = ""
-    l_selvedge = ""
-    knit_or_purl = "k"
-    
-    if (check_valid(punch_card, stitches_per_row, row_number)):
-        x_repeats = calc_repeats(int(stitches_per_row), len(punch_card))
-        y_repeats = calc_repeats(int(row_number), len(punch_card[0]))
-        x_xs_left, x_xs_right = excess_stitch(int(stitches_per_row), len(punch_card), x_position)
-        y_xs_up, y_xs_down = excess_stitch(int(row_number), len(punch_card[0]), y_position)
-
-        if (y_xs_down > 0):
-            instructions += "Knit " + str(y_xs_down) + " rows stockinette \n \n"
-        
-        if (x_xs_right > 0):
-            r_selvedge = " (selvedge: k" + "A" + str(x_xs_right) + ") "
-        if (x_xs_left > 0):
-            l_selvedge = " (selvedge: k"  + "A" + str(x_xs_left) + ") " 
-
-        instructions += "Pattern Start: \n"
-        for i in punch_card:
-             
-            for j in i:
-                if j == previous:
-                    number += 1
-                else:
-                    if j == 0:
-                        if (previous != -1):
-                            row_instructions += knit_or_purl + "B" + str(number) + ", "
-                            number = 1
-                        previous = j
-                    elif j == 1: 
-                        if (previous != -1):
-                            row_instructions += knit_or_purl + "A" + str(number) + ", "
-                            number = 1
-                        previous = j
-            end_catch = "A" if previous%2 == 0 else "B"
-            row_instructions += knit_or_purl + end_catch + str(number)
-            if (x_repeats > 1):
-                instructions += "Row " + str(row) + ": " + l_selvedge + "*" + row_instructions + "* " + str(x_repeats) + " times" + r_selvedge + "\n"
-            else:
-                instructions += "Row " + str(row) + ": " + l_selvedge + row_instructions + r_selvedge + "\n"        
-            knit_or_purl = "k" if row%2 == 0 else "p"
-            previous = -1
-            number = 1
-            row_instructions = ""
-            row +=1
-        if(y_repeats > 1):
-            instructions += "\nRepeat pattern for " + str(y_repeats) + " number of times \n"
-        if (y_xs_up > 0):
-            instructions += "\nKnit " + str(y_xs_up) + " rows in stockinette"
-    else:
-        instructions = "The measurements given do not work. Please choose numbers greater or equal to the number of stitches in the chart"
-    return instructions
-
-"""
-Makes instructions for a cable chart
-"""    
-
-def make_instructions_cable(punch_card, stitches_per_row, row_number):
-    number = 1
-    row = 1
-    previous = -1
-    instructions = ""
-    row_instructions = ""
-    knit_or_purl = "k"
-    for i in punch_card:
-        for j in i:
-            if j == previous:
-                number += 1
-            else:
-                if j == 0:
-                    if (previous != -1):
-                        row_instructions += knit_or_purl + "B" + str(number) + ", "
-                        number = 1
-                    previous = j
-                elif j == 1: 
-                    if (previous != -1):
-                        row_instructions += knit_or_purl + "A" + str(number) + ", "
-                        number = 1
-                    previous = j
-        end_catch = "A" if previous%2 == 0 else "B"
-        row_instructions += knit_or_purl + end_catch + str(number)
-        instructions += "Row " + str(row) + ": " + row_instructions + "\n"
-        knit_or_purl = "k" if row%2 == 0 else "p"
-        previous = -1
-        number = 1
-        row_instructions = ""
-        row +=1
-    print(instructions)
-    return instructions
-
 # Example usage
 #image, punch_card = process_chart("C:/Users/Alabaster/Pictures/knit_chart_color_small.png")
-stitch_matrix, thresh, gray = process_chart("C:/Users/Alabaster/Pictures/knit_chart_cable.png")
-punch_card = cable_punch(stitch_matrix, thresh, gray)
-#make_instructions_lace(punch_card)
+#stitch_matrix, thresh, gray = process_chart("C:/Users/Alabaster/Pictures/knit_chart_lace.png")
+#punch_card = lace_punch(stitch_matrix, thresh, gray)
+#knit_compile.make_instructions_lace(punch_card, 10, 10, "center", "center")
 #determine_chart("C:/Users/Alabaster/Pictures/knit_chart_lace.png")
 #cv2.imshow("Labeled Grid", image)
 #cv2.waitKey(0)
